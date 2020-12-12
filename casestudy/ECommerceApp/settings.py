a46e3a8b5554f739eee96a304b8e56ee9fa28927
jhansi <jhansiravipati2001@gmail.com>
2020-12-12 18:07:28 +0530

    allowing hosts

--- casestudy/ECommerceApp/settings.py
+++ casestudy/ECommerceApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pncii-es@&n!!d)kt5oy!6tgc4wr%+q*sn5hf^))s&2gz(^o8('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['104.198.136.154','34.67.49.220','127.0.0.1', 'localhost', 'testserver',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver',]
 
 
 # Application definition
